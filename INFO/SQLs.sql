655b953ead52307f203e3652b428e46e1ddf6466
Ростислав <ros.artym@gmail.com>
2024-06-14 14:26:41 +0300

    Initial commit

---
+++ INFO/SQLs.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE employees (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    position VARCHAR(50) NOT NULL,
+    phone VARCHAR(15) NOT NULL
+);
+
+
+
